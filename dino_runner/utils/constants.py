4dc80e92e88d6f54df6eecabfd289a31a4baa0df
J0hnmelo <120614402+J0hnmelo@users.noreply.github.com>
2023-05-22 14:45:54 -0300

    alterações e adições

--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -66,7 +66,7 @@ SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
 HAMMER = pygame.image.load(os.path.join(IMG_DIR, 'Other/hammer.png'))
 
 BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
-
+MONTAIN = pygame.image.load(os.path.join(IMG_DIR, 'Other/montain.png'))
 HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
 
 DEFAULT_TYPE = "default"
